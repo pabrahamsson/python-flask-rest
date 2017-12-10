e48906f0ad8e68d888496770de8857962acf5423
Petter Abrahamsson <petter@jebus.nu>
2017-12-09 19:38:58 -0500

    Use get_data(as_text=True) on werkzeuq Response object

--- test/test_wsgi.py
+++ test/test_wsgi.py
@@ -13,7 +13,7 @@ class AppTestEndpoints(unittest.TestCase):
 
     def test_environment_data(self):
         result = self.app.get('/environment')
-        self.assertFalse(json.loads(result.data)['config']['JSONIFY_PRETTYPRINT_REGULAR'])
+        self.assertFalse(json.loads(result.get_data(as_text=True))['config']['JSONIFY_PRETTYPRINT_REGULAR'])
 
     def test_environment_status_code(self):
         result = self.app.get('/environment')
@@ -21,11 +21,11 @@ class AppTestEndpoints(unittest.TestCase):
 
     def test_greeting_data(self):
         result = self.app.get('/greeting')
-        self.assertEqual(json.loads(result.data)['content'], 'Hello, World!')
+        self.assertEqual(json.loads(result.get_data(as_text=True))['content'], 'Hello, World!')
 
     def test_greeting_data_name(self):
         result = self.app.get('/greeting?name=OpenShift')
-        self.assertEqual(json.loads(result.data)['content'], 'Hello, OpenShift!')
+        self.assertEqual(json.loads(result.get_data(as_text=True))['content'], 'Hello, OpenShift!')
 
     def test_greeting_status_code(self):
         result = self.app.get('/greeting')
@@ -33,7 +33,7 @@ class AppTestEndpoints(unittest.TestCase):
 
     def test_health_data(self):
         result = self.app.get('/health')
-        self.assertEqual(json.loads(result.data)['status'], 'success')
+        self.assertEqual(json.loads(result.get_data(as_text=True))['status'], 'success')
 
     def test_health_status_code(self):
         result = self.app.get('/health')
@@ -41,8 +41,8 @@ class AppTestEndpoints(unittest.TestCase):
 
     def test_home_data(self):
         result = self.app.get('/')
-        self.assertEqual(json.loads(result.data)['message'], 'This is my webservice!')
-        self.assertEqual(json.loads(result.data)['paths'], "['/', '/greeting', '/hostinfo']")
+        self.assertEqual(json.loads(result.get_data(as_text=True))['message'], 'This is my webservice!')
+        self.assertEqual(json.loads(result.get_data(as_text=True))['paths'], "['/', '/greeting', '/hostinfo']")
 
     def test_home_status_code(self):
         result = self.app.get('/')
@@ -54,7 +54,7 @@ class AppTestEndpoints(unittest.TestCase):
 
     def test_hostinfo_data(self):
         result = self.app.get('/hostinfo')
-        self.assertIn('hostname', json.loads(result.data))
+        self.assertIn('hostname', json.loads(result.get_data(as_text=True)))
 
 if __name__ == '__main__':
     unittest.main()
